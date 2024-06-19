cd4f40f7b75c7f837bc4f7b20daee3b08f5b0326
risha <rishadjhangiani@gmail.com>
2024-06-19 12:23:06 -0700

    test api

--- popup/popup.js
+++ popup/popup.js
@@ -5,11 +5,30 @@ document.addEventListener("DOMContentLoaded", function() {
     let file = document.getElementById("file");
     let output = document.getElementById("output");
 
-    function logFile (event) {
+    const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    console.log("api!");
+    console.log(xhr.response);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+
+
+    /*function logFile (event) {
         let str = event.target.result;
         let json = JSON.parse(str);
+        let result = JSON.stringify(json, null, 2);
         console.log('string', str);
         console.log('json', json);
+        console.log('json one', result);
+        for(var key in json){
+            console.log(key + ' is ' + result[key]);
+        }
 
         output.textContent = JSON.stringify(json, null, 2);
     }
@@ -23,5 +42,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     form.addEventListener('submit', handleSubmit);
+    */
 });
 
